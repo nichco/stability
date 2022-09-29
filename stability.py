42174c4d837dc2ceb61c43ac318a8c425e8014fa
Nicholas Orndorff <norndorff@live.com>
2022-09-29 11:40:13 -0700

    works now

--- stability.py
+++ stability.py
@@ -5,11 +5,6 @@ class stability(csdl.Model):
     def initialize(self):
         pass
     def define(self):
-
-        # tail volume coefficient
-        # v_h = (l/c_bar)*(s_h/s)
-        # self.register_output('tail_volume_coefficient',v_h)
-
         # bracketed search: solve for l from s_h? in order to balance moments
         model = csdl.Model()
         s_h = model.declare_variable('htail_area')
@@ -17,12 +12,12 @@ class stability(csdl.Model):
         s = model.declare_variable('wing_area')
         cl_h = model.declare_variable('cl_h') # horizontal tail lift coefficient
         h_0 = model.declare_variable('h_0') # fraction distance from wing LE to wing aerodynamic center (h_0*chord = dist)
-        h = model.declare_variable('wing_area') # fraction distance from wing LE to aircraft CG (h*chord = dist)
+        h = model.declare_variable('h') # fraction distance from wing LE to aircraft CG (h*chord = dist)
         cm = model.declare_variable('cm') # wing and fuselage moment coefficient
         cl = model.declare_variable('cl')
         l = model.declare_variable('l') # distance between wing and tail aerodynamic centers
 
-        y = cm + cl*(h - h_0) - (l/c_bar)*(s_h/s)*cl_h
+        y = cm + cl*(h - h_0) - (l/c_bar)*(s_h/s)*cl_h # from Sadraey aircraft design 2012
 
         model.register_output('y', y)
 
@@ -34,36 +29,19 @@ class stability(csdl.Model):
         s = self.declare_variable('wing_area')
         cl_h = self.declare_variable('cl_h') # horizontal tail lift coefficient
         h_0 = self.declare_variable('h_0') # fraction distance from wing LE to wing aerodynamic center (h_0*chord = dist)
-        h = self.declare_variable('wing_area') # fraction distance from wing LE to aircraft CG (h*chord = dist)
+        h = self.declare_variable('h') # fraction distance from wing LE to aircraft CG (h*chord = dist)
         cm = self.declare_variable('cm') # wing and fuselage moment coefficient
         cl = self.declare_variable('cl')
 
         l = solver(s_h,c_bar,s,cl_h,h_0,h,cm,cl)
 
-        # self.register_output('l',1*l)
-        
-class ExampleBracketedScalar(csdl.Model):
-
-    def define(self):
-        model = csdl.Model()
-        a = model.declare_variable('a')
-        b = model.declare_variable('b')
-        c = model.declare_variable('c')
-        x = model.declare_variable('x')
-        y = a * x**2 + b * x + c
-        model.register_output('y', y)
-
-        solve_quadratic = self.create_implicit_operation(model)
-        solve_quadratic.declare_state('x', residual='y', bracket=(0, 2))
-
-        a = self.declare_variable('a', val=1)
-        b = self.declare_variable('b', val=-4)
-        c = self.declare_variable('c', val=3)
-        x = solve_quadratic(a, b, c)
+        # tail volume coefficient
+        v_h = (l/c_bar)*(s_h/s)
+        self.register_output('tail_volume_coefficient',v_h)
 
 
-sim = python_csdl_backend.Simulator(ExampleBracketedScalar())
+sim = python_csdl_backend.Simulator(stability())
 sim.run()
 
-# print('l', sim['l'])
+print('l', sim['l'])
 
